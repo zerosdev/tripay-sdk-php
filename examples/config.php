6798da6b7c225b46ad033e745c213d8957f9fecd
ZerosDev <ronywisnuwardana@gmail.com>
2023-03-02 14:23:01 +0700

    add example

---
+++ examples/config.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /**
+     * Integration mode
+     */
+    'mode' => 'development',
+
+    /**
+     * Merchant code
+     */
+    'merchant_code' => 'T0000',
+
+    /**
+     * Api Key
+     */
+    'api_key' => 'd1cf2f*********c458ed3',
+
+    /**
+     * Private Key
+     */
+    'private_key' => 'd1cf2f*********c458ed3',
+
+    /**
+     * Additional Guzzle options
+     */
+    'guzzle_options' => []
+
+];
